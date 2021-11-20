1a38bd979d1b064fe9e03679524d07724dcc70f5
Kirill Fedyakov <vogelfenix@gmail.com>
2021-11-20 13:52:38 +0100

    Add period parameter to fetch jobs in the last < 31 days

--- src/datafetch/headhunter.py
+++ src/datafetch/headhunter.py
@@ -3,7 +3,7 @@ import configuration.settings
 
 api_base_url = configuration.settings.HEADHUNTER_API_BASE_URL
 
-def fetch_vacancies(text, area=1, professional_role=96, currency='RUR', only_with_salary='true', page=0, per_page=100):
+def fetch_vacancies(text, area=1, professional_role=96, currency='RUR', only_with_salary='true', period=30, page=0, per_page=100):
     """Fetch vacancies from HeadHunter API, return in json"""
     url = f'{api_base_url}/vacancies'
     url_params = {
@@ -12,11 +12,13 @@ def fetch_vacancies(text, area=1, professional_role=96, currency='RUR', only_wit
         'professional_role': professional_role, 
         'currency': currency,
         'only_with_salary': only_with_salary,
+        'period': period,
         'page': page,
         'per_page': per_page,
     }
 
     response = requests.get(url, url_params)
+    # print(response.url)
     response.raise_for_status()
 
     return response.json()
--- src/main.py
+++ src/main.py
@@ -4,20 +4,28 @@ import json
 pp = json.dumps
 
 def main():
-    pages = headhunter.fetch_vacancies('Python', currency='EUR')['pages']
+    search_parameters = {
+        'text':'Python Germany', 
+        'period': 30,
+    }
+    pages = headhunter.fetch_vacancies(**search_parameters)['pages']
     
     paginated_vacancies = {}
     for page in range(pages):
-        vacancies_per_page = headhunter.fetch_vacancies('Python', currency='EUR', page=page)
+        vacancies_per_page = headhunter.fetch_vacancies(**search_parameters, page=page)
         paginated_vacancies[page] = vacancies_per_page
     # print(pp(paginated_vacancies, indent=4))
 
     counter = 1
     for vacancies_per_page in paginated_vacancies.values():
         for vacancy in vacancies_per_page['items']:
-            if vacancy['salary']['currency'] == 'EUR':
-                print(f'{counter}: {vacancy["name"]} -- {vacancy["alternate_url"]}' , )
+            if vacancy['salary']['currency'] == 'RUR':
+                # vacancy['salary']['currency']
+                print(f'{counter}: {vacancy["salary"]["from"]} - {vacancy["salary"]["to"]} -> {vacancy["name"]} -- {vacancy["alternate_url"]}' , )
                 counter += 1
+            # if not vacancy['department']:
+            #     print(f'{counter}: {vacancy["department"]} -> {vacancy["name"]} -- {vacancy["alternate_url"]}' , )
+            #     counter += 1
 
 if __name__ == '__main__':
     main()
